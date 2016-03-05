d003d17c68a712c034d0d0c6b4be92f92a9f302a
Sascha Schlindwein <schlind@users.noreply.github.com>
2016-03-05 19:01:37 +0100

    Update tty1-clock.sh

--- bin/tty1-clock.sh
+++ bin/tty1-clock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 display="/dev/tty1"
-#display=$(tty)
+display=$(tty)
 function print_datetime {
  echo  > $display
  echo -en "\e[1m\e[32m" > $display
@@ -19,25 +19,20 @@ function print_datetime {
  echo -en "\e[0m" > $display
 }
 function print_second {
- if [ $(expr $i % 20) -eq 0 ]; then
-  echo -en "\e[1m\e[33m $i\e[0m" > $display
-  echo > $display
- elif [ $(expr $i % 10) -eq 0 ]; then
-  echo -en "\e[1m\e[36m $i\e[0m" > $display
+ if [ $(expr $1 % 10) -eq 0 ]; then
+  echo -en "\e[1m\e[33m$1\e[0m " > $display
+  #echo > $display
+ elif [ $(expr $1 % 5) -eq 0 ]; then
+  echo -en "\e[1m\e[36m$1\e[0m " > $display
  else
-  echo -en "\e[1m\e[32m $i\e[0m" > $display
+  echo -en "\e[1m\e[32m$1\e[0m " > $display
  fi
 }
 function print_seconds {
- second=$((10#$(date +%S)))
- for (( i=1; i <= $second; ++i )); do print_second $i; done
+ now=$((10#$(date +%S)))
+ for (( second=1; second <= $now; ++second )); do print_second $second; done
  second=$(expr $second + 1)
- for (( i=$second; i <=59; ++i )); do print_second $i; sleep 1; done
-}
-function print_full_minute_action {
- cmatrix -o -b -u 2 -C white >  $display &
- sleep 2
- killall -q cmatrix
+ for (( ; second <=59; ++second )); do print_second $second; sleep 1; done
 }
 function print_reset {
  clear > $display
@@ -47,5 +42,4 @@ while [ 1 ]; do
  print_reset
  print_datetime
  print_seconds
- print_full_minute_action
 done
